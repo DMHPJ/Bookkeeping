3d29599275a10e56bba2764d5d617a50b1da6bce
DMHPJ <2020625277@qq.com>
2025-12-26 12:38:53 +0800

    提取color为colors类

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xff2965FF);
+  static const Color textGray = Color(0xFF666666);
+  static const Color iconGray = Color(0xFF999999);
+  
+  // Backgrounds
+  static const Color inputFill = Color(0xFFEEEEEE);
+  static const Color inputFillLight = Color(0xFFF2F2F2);
+  
+  // Shadows
+  static const Color shadowBlue = Color.fromRGBO(41, 101, 255, 0.06);
+  
+  // Text
+  static const Color white = Colors.white;
+}
